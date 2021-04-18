befc69576a1051fdead04ba6fdb66e41cf79e2a3
Abraham E. Tavarez <abeincwireless@gmail.com>
2021-04-17 23:34:41 -0400

    day one sandbox

---
+++ Day_1/sandbox.py
@@ -0,0 +1,13 @@
+######### Printing / Strings###################
+
+# Printing
+print('Day One')
+print("Day One")
+
+# Printing in new line
+print('Hello World!\nHello World!')
+
+# Concatination
+print('Hello' + 'User') # print all together
+print('Hello' + ' ' + 'User')   # print with added space
+print('Hello ' + 'User')    # added space to first word
