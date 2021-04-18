af41490f686a01b32e1c424d5c80851e8d83a232
Abraham E. Tavarez <abeincwireless@gmail.com>
2021-04-17 23:42:17 -0400

    day one sandbox

--- Day_1/sandbox.py
+++ Day_1/sandbox.py
@@ -7,7 +7,13 @@ print("Day One")
 # Printing in new line
 print('Hello World!\nHello World!')
 
-# Concatination
+# Concatenation
 print('Hello' + 'User') # print all together
 print('Hello' + ' ' + 'User')   # print with added space
 print('Hello ' + 'User')    # added space to first word
+
+########## Input Function #############
+
+
+username = input('Whats you name? ')    # prompt user for an input
+print('Welcome ' + username)    # print greeting to the user
