47d4d011169248dc083de48c83d3900d5ca6590c
Abraham E. Tavarez <abeincwireless@gmail.com>
2021-04-18 23:07:23 -0400

    day 3 if statements

---
+++ Day_3/sandbox.py
@@ -0,0 +1,8 @@
+######### EVEN OR ODD ###########
+
+number_input = int(input('Which number you want to check? '))   # User input
+
+if number_input % 2 == 0:
+    print(f'{number_input} is Even!')   # if number is even
+else:
+    print(f'{number_input} is ODD!')    # if number is odd
