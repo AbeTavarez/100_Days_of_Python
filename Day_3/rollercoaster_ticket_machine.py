e877c49ef5ec9256923a0d2f6ac4a3e8798898d4
Abraham E. Tavarez <abeincwireless@gmail.com>
2021-04-18 23:21:37 -0400

    day 3 rollercoaster

---
+++ Day_3/rollercoaster_ticket_machine.py
@@ -0,0 +1,17 @@
+######### ROLLERCOASTER TICKET MACHINE ###############
+
+print('##### Welcome to Rollercoaster! ########')
+
+height = float(input('What is your height? '))  # user input
+
+if height >= 40:
+    print('You can ride the rollercoaster!')    # true if height is more or equal to 40
+    age = int(input('What is your age? '))
+    if age <= 18:
+        print('Please pay $7.00')   # true if age is less or equal to 18
+    else:
+        print('Please pay $12.00')  # false if age is more than 18
+else:
+    print('Sorry, you still not tall enough to ride!')  # false if height is less than 40
+
+
